54e818dd0e5f245756cadf7cbc0e64ef0bec8974
Michael Brown <brownxmi@amazon.com>
2019-05-09 10:33:42 -0700

    adding an initial script to setup and install docker

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y \
+    emacs25 \
+    bash \
+    git \
+    zsh
+
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update -y &&\
+    apt install -y \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
