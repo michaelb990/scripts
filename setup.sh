c82f1ae7614db5918c68fa1de2ba205e9f0d7a1b
Ubuntu <ubuntu@ip-172-31-9-133.us-west-2.compute.internal>
2019-12-03 20:00:10 +0000

    Update setup script

--- setup.sh
+++ setup.sh
@@ -25,10 +25,12 @@ sudo add-apt-repository \
    stable"
 
 sudo apt update -y &&\
-    apt install -y \
+    sudo apt install -y \
         docker-ce \
         docker-ce-cli \
         containerd.io
 
-sudo groupadd docker
 sudo usermod -aG docker $USER
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
